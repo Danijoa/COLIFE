eaa5014835f33fe25e407e6ac62f87d5be519ad5
SeungYeon <yeondiary@gmail.com>
2020-03-13 17:11:32 +0900

    달력 추가

--- screens/RecentScreen.js
+++ screens/RecentScreen.js
@@ -1,22 +1,123 @@
-import *as React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {Component} from 'react';
+import {Alert, StyleSheet, View, ScrollView, Text, TouchableOpacity} from 'react-native';
+import {Calendar, Agenda} from 'react-native-calendars';
+import {LocaleConfig} from 'react-native-calendars';
 
-class RecentScreen extends React.Component {
+LocaleConfig.locales['ko'] = {
+  monthNames: ['1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월'],
+  monthNamesShort: ['1','2','3','4','5','6','7.','8','9','10','11','12'],
+  dayNames: ['일요일','월요일','화요일','수요일','목요일','금요일','토요일'],
+  dayNamesShort: ['일','월','화','수','목','금','토'],
+  today: 'Aujourd\'hui'
+};
+LocaleConfig.defaultLocale = 'ko';
+
+export default class CalendarsScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: undefined,
+      items: {}
+    };
+  }
+
+  onDayPress = (day) => {
+    this.setState({selected: day.dateString});
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Recent Todo List</Text>
+      <Calendar
+      style={styles.calendar}
+      hideExtraDays
+      onDayPress={this.onDayPress}
+      markedDates={{
+        [this.state.selected]: {
+          selected: true, 
+          disableTouchEvent: true, 
+          selectedDotColor: 'orange'
+        }
+      }}
+    />
+    );
+  }
+
+  loadItems(day) {
+    setTimeout(() => {
+      for (let i = 0; i < 1; i++) {
+        const time = day.timestamp + i * 24 * 60 * 60 * 1000;
+        const strTime = this.timeToString(time);
+        if (!this.state.items[strTime]) {
+          this.state.items[strTime] = [];
+          const numItems = Math.floor(Math.random() * 5);
+          for (let j = 0; j < numItems; j++) {
+            this.state.items[strTime].push({
+              name: strTime + ' #' + j,
+              height: Math.max(50, Math.floor(Math.random() * 150))
+            });
+          }
+        }
+      }
+      const newItems = {};
+      Object.keys(this.state.items).forEach(key => {newItems[key] = this.state.items[key];});
+      this.setState({
+        items: newItems
+      });
+    }, 1000);
+  }
+
+  renderItem(item) {
+    return (
+      <TouchableOpacity 
+        style={[styles.item, {height: item.height}]} 
+        onPress={() => Alert.alert(item.name)}
+      >
+        <Text>{item.name}</Text>
+      </TouchableOpacity>
+    );
+  }
+
+  renderEmptyDate() {
+    return (
+      <View style={styles.emptyDate}>
+        <Text>등록된 일정이 없습니다</Text>
       </View>
     );
   }
+
+  rowHasChanged(r1, r2) {
+    return r1.name !== r2.name;
+  }
+
+  timeToString(time) {
+    const date = new Date(time);
+    return date.toISOString().split('T')[0];
+  }
+
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
 
-export default RecentScreen;
+const styles = StyleSheet.create({
+  calendar: {
+    marginBottom: 10
+  },
+  text: {
+    textAlign: 'center',
+    padding: 10,
+    backgroundColor: 'lightgrey',
+    fontSize: 16
+  },
+  item: {
+    backgroundColor: 'white',
+    flex: 1,
+    borderRadius: 5,
+    padding: 10,
+    marginRight: 10,
+    marginTop: 17
+  },
+  emptyDate: {
+    height: 15,
+    flex:1,
+    paddingTop: 30
+  }
+});
